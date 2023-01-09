f5ba8a5cc5fba690e5cfa372ff717db164573f9c
weedii <abidiw293@gmail.com>
2023-01-09 20:46:46 +0100

    inserts node left-child

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,7 +13,7 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = NULL;
+	binary_tree_t *new_node;
 
 	if (!parent || !new_node)
 		return (NULL);
