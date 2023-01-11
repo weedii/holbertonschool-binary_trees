7112d5f88811f2c907bf6b49e33e32c9777575ee
Aminekham <lkham0508@gmail.com>
2023-01-11 12:21:29 +0100

    leaves of binary tree

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,15 +8,15 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    if (!tree)
-    {
-        return (0);
-    }
+	if (!tree)
+	{
+		return (0);
+	}
 
-    if (!tree->left && !tree->right)
-    {
-        return (1);
-    }
+	if (!tree->left && !tree->right)
+	{
+		return (1);
+	}
 
-    return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
