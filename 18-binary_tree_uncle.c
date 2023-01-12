23b0a4698cd03c99e5067127b2e155fac1eb675b
weedii <abidiw293@gmail.com>
2023-01-12 22:28:24 +0100

    the uncle of a node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -29,11 +29,12 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	binary_tree_t *parent = node->parent;
+	binary_tree_t *parent = NULL;
 
 	if (!node || !node->parent)
 		return (NULL);
 
+	parent = node->parent;
 	if (parent->left == node)
 		return (parent->right);
 	return (parent->left);
